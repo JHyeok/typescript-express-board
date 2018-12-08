9006a2f73896278d2f74ef90648c07845ade44ae
JHyeok <kjh2522k@gmail.com>
2018-12-09 03:25:31 +0900

    fix error handler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import lineRouter from './routes/line';
 
+interface Err extends Error {
+  status: number;
+  data?: any;
+}
+
 class App {
   public app: express.Application;
 
@@ -9,6 +14,7 @@ class App {
     this.app = express();
     this.config();
     this.router();
+    this.errorHandler();
   }
 
   private config(): void {
@@ -16,37 +22,29 @@ class App {
     this.app.use(bodyParser.urlencoded({ extended: false}));
   }
 
+  private errorHandler(): void {
+    this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+      const err = new Error('Not Found') as Err;
+      err.status = 404;
+      next(err);
+    });
+
+    // error handler
+    this.app.use((err: Err, req: express.Request, res: express.Response, next: express.NextFunction) => {
+      res.status(err.status || 500);
+      res.json({
+        message: err.message,
+        data: err.data,
+      });
+    });
+  }
+
   private router(): void {
     this.app.use('/', lineRouter);
     this.app.get('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
       res.send('Hello, TypeScript-express');
-      console.log('done!');
     });
   }
 }
 
 export default App;
-
-// TODO : error handler 적용
-/*
-interface Err extends Error {
-  status: number;
-  data?: any;
-}
-
-// 404 error 잡아서 error handler 로 전달
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const err = new Error('Not Found') as Err;
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use((err: Err, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    data: err.data,
-  });
-});
-*/
